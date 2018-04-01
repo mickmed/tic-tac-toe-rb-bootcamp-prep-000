a7fc38265b6fc5bf85919b8fad28cc2129713903
Michael Roth <mick2090@gmail.com>
2018-04-01 17:33:20 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -115,7 +115,7 @@ def play(board)
     turn(board)
   end
   if won?(board)
-    puts "Congratulations #{winner}!"
+    puts "Congratulations #{winner(board}!"
   elsif draw?(board)
     puts "Cat's Game!"
   end
